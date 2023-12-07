0f1c9c36e7703178dbe527c85e7e240e9bd668d9
cadieuxpaquinr <cadieuxpaquin.r@gmail.com>
2023-12-07 03:25:47 -0500

    Buttons action

---
+++ scripts.js
@@ -0,0 +1,24 @@
+document.getElementById('homeBtn').addEventListener('click', function() {
+    alert('Home button clicked!');
+    // Add your logic or page navigation code here
+});
+
+document.getElementById('projectsBtn').addEventListener('click', function() {
+    alert('Projects button clicked!');
+    // Add your logic or page navigation code here
+});
+
+document.getElementById('aboutMeBtn').addEventListener('click', function() {
+    alert('About me button clicked!');
+    // Add your logic or page navigation code here
+});
+
+document.getElementById('contactBtn').addEventListener('click', function() {
+    alert('Contact me button clicked!');
+    // Add your logic or page navigation code here
+});
+
+document.getElementById('frenchBtn').addEventListener('click', function() {
+    alert('French button clicked!');
+    // Add your logic or page navigation code here
+});
